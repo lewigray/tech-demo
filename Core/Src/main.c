b4f6e59d1566ba2fb6162be6c50229e4e6a9c26b
graylew <graylew@users.noreply.github.com>
2021-11-24 02:00:07 -0800

    Got data from SPI bus
    it was two switches... kill me

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -36,15 +36,43 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define CS_PORT 	GPIOD
+#define CS_PIN		GPIO_PIN_14
+
+#define DRDY_PORT 	GPIOE
+#define DRDY_PIN	GPIO_PIN_13
+
+#define RST_PORT	GPIOF
+#define RST_PIN		GPIO_PIN_14
+
+#define PRE			0xFA
+#define	BID			0xFF
+
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 
+void flush_buffer(uint8_t* buf, int len) {
+	for (int i = 0; i < len; i++) {
+		buf[i] = 0;
+	}
+}
+
+void reset_xsens() {
+	HAL_GPIO_WritePin(RST_PORT, RST_PIN, GPIO_PIN_SET);
+	HAL_Delay(10);
+	HAL_GPIO_WritePin(RST_PORT, RST_PIN, GPIO_PIN_RESET);
+	HAL_Delay(100);
+}
+
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef hlpuart1;
+
+SPI_HandleTypeDef hspi1;
 
 /* USER CODE BEGIN PV */
 
@@ -54,6 +82,7 @@ UART_HandleTypeDef hlpuart1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_LPUART1_UART_Init(void);
+static void MX_SPI1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -73,6 +102,12 @@ int main(void)
 
 	uint8_t buf[64];
 	int counter = 0;
+
+	uint8_t spi_tx_buf[64];
+	uint8_t spi_rx_buf[64];
+
+	flush_buffer(spi_tx_buf, 64);
+	flush_buffer(spi_rx_buf, 64);
 
   /* USER CODE END 1 */
 
@@ -95,7 +130,15 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_LPUART1_UART_Init();
+  MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
+
+  //FA FF 30 00 D1
+  spi_tx_buf[0] = PRE;
+  spi_tx_buf[1] = BID;
+  spi_tx_buf[2] = 0x30;
+  spi_tx_buf[3] = 0x00;
+  spi_tx_buf[4] = 0xD1;
 
   /* USER CODE END 2 */
 
@@ -103,15 +146,47 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-
 	  if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13)) {
-		  HAL_Delay(25);
-		  sprintf((char*)buf, "Button Pressed %d Times!\r\n", counter);
-		  HAL_UART_Transmit(&hlpuart1, buf, strlen((char*)buf), HAL_MAX_DELAY);
-		  while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));
-		  counter++;
+	  		  HAL_Delay(25);
+	  		  sprintf((char*)buf, "Button Pressed %d Times!\r\n", counter);
+	  		  HAL_UART_Transmit(&hlpuart1, buf, strlen((char*)buf), HAL_MAX_DELAY);
+
+	  		  reset_xsens();
+
+	  		  while(!HAL_GPIO_ReadPin(DRDY_PORT, DRDY_PIN));
+
+	  		  HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_RESET);
+	  		  HAL_SPI_Transmit(&hspi1, (uint8_t*) spi_tx_buf, 5, 100);
+	  		  //HAL_SPI_Receive(&hspi1, (uint8_t*) spi_rx_buf, 5, 100);
+	  		  HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_SET);
+
+	  		  flush_buffer(spi_tx_buf, 64);
+
+	  		  spi_tx_buf[0] = PRE;
+	  		  spi_tx_buf[1] = BID;
+	  		  spi_tx_buf[2] = 0x00;
+	  		  spi_tx_buf[3] = 0x00;
+	  		  spi_tx_buf[4] = 0x01;
+
+	  		  HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_RESET);
+	  		  HAL_SPI_Transmit(&hspi1, (uint8_t*) spi_tx_buf, 5, 100);
+	  		  HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_SET);
+
+	  		  while(!HAL_GPIO_ReadPin(DRDY_PORT, DRDY_PIN));
+
+	  		  HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_RESET);
+	  		  HAL_SPI_Receive(&hspi1, (uint8_t*) spi_rx_buf, 64, 100);
+	  		  HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_SET);
+
+	  		  for(int i = 0; i < 64; i++) {
+	  			  sprintf((char*)buf, "0x%02x\r\n", (uint8_t) spi_rx_buf[i]);
+	  			  HAL_UART_Transmit(&hlpuart1, buf, strlen((char*)buf), HAL_MAX_DELAY);
+	  		  }
+
+	  		  while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));
+	  		  counter++;
 	  }
+    /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
   }
@@ -201,6 +276,46 @@ static void MX_LPUART1_UART_Init(void)
 }
 
 /**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 7;
+  hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi1.Init.NSSPMode = SPI_NSS_PULSE_DISABLE;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -212,13 +327,22 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOG_CLK_ENABLE();
   HAL_PWREx_EnableVddIO2();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOF, GPIO_PIN_14, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, LD3_Pin|LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOG, USB_PowerSwitchOn_Pin|SMPS_V1_Pin|SMPS_EN_Pin|SMPS_SW_Pin, GPIO_PIN_RESET);
@@ -229,12 +353,32 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PF14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PE13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
   /*Configure GPIO pins : LD3_Pin LD2_Pin */
   GPIO_InitStruct.Pin = LD3_Pin|LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PD14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
   /*Configure GPIO pins : USB_OverCurrent_Pin SMPS_PG_Pin */
   GPIO_InitStruct.Pin = USB_OverCurrent_Pin|SMPS_PG_Pin;
